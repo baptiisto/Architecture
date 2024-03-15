fd9d8b0d9696a17e88804dfa461c32bc4c9a4a2a
baptiisto <baptisto78@gmail.com>
2024-03-15 14:09:20 +0100

    jsp

--- toudou_cli_starter/src/toudou/__init__.py
+++ toudou_cli_starter/src/toudou/__init__.py
@@ -1,5 +1,16 @@
 import os
+import logging
+import sys
+
 config = dict(
 DATABASE_URL=os.getenv("TOUDOU_DATABASE_URL", ""),
 DEBUG=os.getenv("TOUDOU_DEBUG", "False") == "True",
+)
+logging.basicConfig(
+level=logging.INFO,
+format="%(asctime)s [%(levelname)s] %(message)s",
+handlers=[
+logging.FileHandler("toudou.log"),
+logging.StreamHandler(stream=sys.stdout)
+]
 )
